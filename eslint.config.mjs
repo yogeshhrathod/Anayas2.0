b2f58e9e303b2b696a3920226bc70932710b52ec
Yogesh Rathod <yrathod@qualys.com>
2025-11-27 14:46:51 +0530

    lint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,6 +29,7 @@ export default tseslint.config(
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
         },
       ],
       '@typescript-eslint/explicit-module-boundary-types': 'off',
